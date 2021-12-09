0e8b1bf0901367b3e31c067123ba39c0b24bd9e8
Phillips III, Ernest Alvin CTR USA <ernest.a.phillips4.ctr@mail.mil>
2021-12-09 09:50:38 -0500

    Changed the content_main to fragment_main and created a fragement class to handle load of xml file. I plan to work out of the nav drawer, so everything will be handled through fragments.
    A frame layout has been added and directly referenced in the app_bar_main. Here all fragments will be injected, starting with main on app load, or possibly a login if I decide to do that.

---
+++ WeightApp/Fragments/MainFragment.cs
@@ -0,0 +1,26 @@
+﻿using Android.OS;
+using Android.Views;
+
+namespace WeightApp.Fragments
+{
+  /*
+   * Ernie Phillips III : 12/09/2021
+   * Main fragment to display when user first opens app
+   */
+
+  public class MainFragment : AndroidX.Fragment.App.Fragment
+  {
+    public override void OnCreate(Bundle savedInstanceState)
+    {
+      base.OnCreate(savedInstanceState);
+
+      // Create your fragment here
+    }
+
+    public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+      // Use this to return your custom view for this Fragment
+      return inflater.Inflate(Resource.Layout.fragment_main, container, false);
+    }
+  }
+}
